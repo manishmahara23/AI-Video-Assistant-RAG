b45af9454c36a441833f597e3f4a18d9842d44de
Manish Mahara <manish23mahara@gmail.com>
2026-07-13 00:44:20 +0530

    audio processor file updated

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -36,7 +36,7 @@ def download_youtube_audio(url: str) -> str:
     cookie_file = _create_cookie_file()
 
     ydl_opts = {
-        "format": "bestaudio/bestaudio*/best",
+        "format": "140/251/250/249/bestaudio/best",
         "outtmpl": output_path,
         "quiet": True,
         "no_warnings": True,
